72d2330d3abaab1ff8282437c32e5d7f0cbdf0f4
vincentlee1218 <44049108+vincentlee1218@users.noreply.github.com>
2019-11-18 15:13:00 -0800

    Amazing backup
    First backup completed

---
+++ MovieListAL.java
@@ -0,0 +1,74 @@
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Write a description of class MovieListAL here.
+ *
+ * @author (Mr. Kim)
+ * @version (2019-11-16)
+ */
+public class MovieListAL
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<Movie> pool;
+
+    /**
+     * Constructor for objects of class MovieListsAL
+     */
+    public MovieListAL(Movie[] movies)
+    {
+        pool = new ArrayList<Movie>();
+        for (int i = 0; i < movies.length; i++) pool.add(movies[i]);
+    }
+
+    public ArrayList<Movie> getPool()
+    {
+        return pool;
+    }
+    
+    /**
+     * Given an arraylist of Movies and a studio name, create a new ArrayList of movies
+     * by that studio
+     */
+    public ArrayList<Movie> getByStudio(String studio)
+    {
+        ArrayList<Movie> movieRes = new ArrayList<Movie>();
+        for (int i = 0; i < pool.size(); i++)
+        if (pool.get(i).getStudio().equals(studio)) movieRes.add(pool.get(i));
+        return movieRes;
+    }
+    
+    
+    /** 
+     * get the movie with the highest rating within an ArrayList
+     */
+    public Movie getHighestrating(ArrayList<Movie> movies)
+    {
+        if (movies.size() == 0) return null;
+        double highestRating = movies.get(0).getRating();
+        int highestIndex=0;
+        for (int i = 0; i < movies.size(); i++) {
+            double x = movies.get(i).getRating();
+            if (x > highestRating) { highestRating = x; highestIndex = i; }
+        }
+        return movies.get(highestIndex);
+        
+    }
+    
+    
+
+    
+    /**
+     * Find the highest movies by studio
+     */
+    public ArrayList<Movie> findHighestRatedByStudio()
+    {
+        ArrayList<Movie> highestRated = new ArrayList<Movie>();
+        
+        // Your code goes here
+         return null;
+        // return highestRated;
+        
+    }
+
+}
